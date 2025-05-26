85f9e7eb4ab054db33a8c8b628dfb5351dc862e5
BuwieSit <santosbuhawie28@gmail.com>
2025-05-26 19:54:25 +0800

    account animation okay na to

--- globalScript.js
+++ globalScript.js
@@ -8,6 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.querySelector('.toggle');
     const accountBtn = document.getElementById('accButton');
     const popup = document.querySelector('.acc-popup');
+    const pleft = document.querySelector('.pleft');
+    const pright = document.querySelector('.pright');
+
 
     accountBtn.addEventListener('click', () => {
         if (popup.style.opacity == '0') {
@@ -25,11 +28,19 @@ window.addEventListener('DOMContentLoaded', () => {
             slider.style.transform = 'translateX(-50%)';
             toggleBtn.textContent = 'Register';
             slider.style.boxShadow = '10px 0px 10px black';
+            pleft.style.opacity = '0';
+            pleft.style.transform = 'scale(0.1)'
+            pright.style.transform = 'scale(1)'
+            pright.style.opacity = '1';
         }
         else {
             slider.style.transform = 'translateX(50%)'; //if slider is in the left side
             toggleBtn.textContent = 'Login';
             slider.style.boxShadow = '-10px 0px 10px black';
+            pright.style.opacity = '0';
+            pright.style.transform = 'scale(0.1)'
+            pleft.style.transform = 'scale(1)'
+            pleft.style.opacity = '1';
         }
         
     });
