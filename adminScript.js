5b5476c94c9d5445ea795d540a42374d24764f4f
BuwieSit <santosbuhawie28@gmail.com>
2025-05-30 18:59:00 +0800

    php db (unfinished) and admin page

---
+++ adminScript.js
@@ -0,0 +1,17 @@
+  window.addEventListener('DOMContentLoaded', () => {
+    
+    const adminToggle = document.querySelector('.admin-toggle-btn');
+    const signup = document.querySelector('.signUp-wrapper');
+    const signin = document.querySelector('.signIn-wrapper');
+    
+    
+
+
+    adminToggle.addEventListener('click', () => {
+
+        console.log('clicked');
+    });
+   
+  });
+
+
