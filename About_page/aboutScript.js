22c0fb38d215f88a9511d0883b23170130d73cd4
BuwieSit <santosbuhawie28@gmail.com>
2025-05-31 15:04:08 +0800

    about page done, starting on landing page

--- About_page/aboutScript.js
+++ About_page/aboutScript.js
@@ -9,3 +9,18 @@ if (barangays.length > 0) {
     });
 }
 
+const dropdownBtn = document.querySelector('.dropdown-button');
+const barangay_list = document.querySelector('.barangay-list');
+
+dropdownBtn.addEventListener('click', () => {
+    
+
+    if(barangay_list.style.height <= '50px') {
+        barangay_list.style.height = '80%';
+        barangay_list.style.overflowY = 'scroll';
+    }
+    else {
+        barangay_list.style.height = '50px';
+        barangay_list.style.overflowY = 'hidden';
+    }
+});
