a173d21c4f5cf9347f63e51b99c3fede49926663
BuwieSit <santosbuhawie28@gmail.com>
2025-06-01 05:05:00 +0800

    js sa permits

--- Services_page/permitScript.js
+++ Services_page/permitScript.js
@@ -1,10 +1,27 @@
+//POPUP FUNCTIONALITY
+const permit_text = document.querySelector('.permit-text');
+const container = document.querySelector('.sys-container');
+const popup_text = document.querySelector('.popup-text');
+
+function changeText() {
+  let text = permit_text.textContent.trim();
+  popup_text.textContent = text;
+}
+
+
+
 function openPopup(idToOpen, idToClose = null) {
   if (idToClose) {
     document.getElementById(idToClose).classList.remove("open-popup");
   }
+  
   document.getElementById(idToOpen).classList.add("open-popup");
+  changeText();
 }
 
 function closePopup(idToClose) {
   document.getElementById(idToClose).classList.remove("open-popup");
 }
+
+
+
