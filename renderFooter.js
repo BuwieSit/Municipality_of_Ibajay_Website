80242e145d8ff28ed13681336f31d0c5ddeb1bf7
BuwieSit <santosbuhawie28@gmail.com>
2025-06-22 19:53:47 +0800

    footer and header done

--- renderFooter.js
+++ renderFooter.js
@@ -11,12 +11,11 @@ function routing() {
 
   const footContents = `
        <footer class = "footer" id="foot">
-            
+          <div class="footer-row ffrow">
             <section class="logo-text">
-                <img src= "${prefix}z-resources/ibajay_logo.png" style = "width: 150px; height: auto;" />
+                <img id="fLogo" src= "${prefix}z-resources/ibajay_logo.png"/>
                 <h1 id="footerTitle">Municipality of Ibajay</h1>
             </section>
-
             <section class = "paragraph">
                 <p>Ibajay lies in the northwest section of Aklan province on Panay Island. It is 
                 bounded on the north by the Sibuyan Sea, east by Tangalan, west by Nabas, and south
@@ -25,9 +24,74 @@ function routing() {
                  16 kilometres (9.9 mi) of National Road, 32.918 kilometres (20.454 mi) of Provincial 
                  Roads, and 75.430 kilometres (46.870 mi) of Barangay Road.
                  </p>
-            
             </section>
+          </div>
+          <div class="footer-row fsrow">
+            <div class="frow-list">
+
+              <div class="frow-wrapper">
+                <p class="frow-titles">Useful Links</p>
+                <ul>
+                  <li><a href="${prefix}News_page/news.html">News</a></li>
+                  <li><a href="${prefix}Services_page/services.html">Services</a></li>
+                  <li><a href="${prefix}About_page/about.html">About</a></li>
+                  <li><a href="${prefix}Contacts_page/contact.html">Contact</a></li>
+                </ul>
+              </div>
              
+              <div class="frow-wrapper">
+                <p class="frow-titles">Services</p>
+                <ul>
+                  <li><a href="${prefix}Services_page/permits.html">Business Permits</a></li>
+                  <li><a href="${prefix}Services_page/healthcare.html">Healthcare</a></li>
+                  <li><a href="${prefix}Services_page/tourism.html">Tourism</a></li>
+                </ul>
+              </div>
+
+              <div class="frow-wrapper">
+                <p class="frow-titles">Updates</p>
+                <ul>
+                  <li><a href="${prefix}News_page/announcement.html">Announcements</a></li>
+                  <li><a>Projects</a></li>
+                  <li><a>Bulletins</a></li>
+                  <li><a>Advisories</a></li>
+                </ul>
+              </div>
+
+
+          </div>
+
+            <div class="frow-sub-cont">
+                <p class="frow-titles">Subscribe</p>
+                <p>Join our community to receive updates!</p>
+                <form class="sub-form">
+                    <button class="sub-button">SUBSCRIBE</button>
+                </form>
+                
+            </div>
+          
+          </div>
+
+          <div class="footer-row ftrow">
+              <section class="ftrow-sect ftrow-left">
+                  <p class="ftrow-title">Ibajay.</p>
+                  <ul class="terms-list">
+                    <li><a>Privacy Policy</a><li>
+                    <li><a>Terms of Service</a><li>
+                    <li><a>Cookie Policy</a><li> 
+                  </ul>
+              </section>
+
+              <section class="ftrow-sect ftrow-right">
+                  <ul class="socials-list">
+                    <li><a><img src= "${prefix}z-resources/ibajay_logo.png"></a></li>
+                    <li><a><img src= "${prefix}z-resources/ibajay_logo.png"></a></li>
+                    <li><a><img src= "${prefix}z-resources/ibajay_logo.png"></a></li>
+                  </ul>
+
+                  <p class="copyright">&copy2025 Team 1 Web Development Team. All rights reserved.</p>
+              </section>
+          </div> 
            
         </footer>
   `;
